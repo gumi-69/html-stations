f117a0291cd0f694eb955b5a6e678b9d5e0383d6
gumi-69 <irohaloid10963@gmail.com>
2024-06-17 15:26:36 +0900

    station15のクリア条件の達成

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const modifiedList = userList.map(user => {
+        user.full_name = `${user.family_name} ${user.first_name}`;
+        return user;
+      });
+      resolve(modifiedList);
+    }, 3000);
+  });
 }
 
+getData().then(result => console.log(result));
